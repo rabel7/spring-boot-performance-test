4e2b18f9a521ddd63e3fa8020a7601e2af47cbf1
daive <52926462@qq.com>
2017-07-27 06:59:02 +0800

    boot docker init

---
+++ bootdocker/src/main/java/cn/com/aperfect/controller/DockerTestController.java
@@ -0,0 +1,20 @@
+package cn.com.aperfect.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerTestController {
+
+    /**
+     * 不能包含{}
+     * @param str
+     * @return
+     */
+    @RequestMapping("/1kString")
+    public String test1kString(@RequestBody String str) {
+        return str;
+    }
+
+}
