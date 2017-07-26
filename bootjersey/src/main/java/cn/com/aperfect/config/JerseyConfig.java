078426882c77d5f2f3737fad6d8d6296f14cdbef
daive <52926462@qq.com>
2017-07-26 22:14:20 +0800

    jersy init

---
+++ bootjersey/src/main/java/cn/com/aperfect/config/JerseyConfig.java
@@ -0,0 +1,16 @@
+package cn.com.aperfect.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+
+/**
+ * Created by daive on 2017/7/26.
+ */
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(RequestContextFilter.class);
+        //配置restful package.
+        packages("cn.com.aperfect.controller");
+    }
+}
--- bootjersey/src/main/java/cn/com/aperfect/controller/JersyTestController.java
+++ bootjersey/src/main/java/cn/com/aperfect/controller/JersyTestController.java
@@ -1,20 +1,29 @@
 package cn.com.aperfect.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
+@Component
 public class JersyTestController {
 
-    /**
-     * 不能包含{}
-     * @param str
-     * @return
-     */
-    @RequestMapping("/1kString")
-    public String test1kString(@RequestBody String str) {
-        return str;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/hello")
+    public Map<String, Object> hello() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", "1");
+        map.put("codeMsg", "success");
+        return map;
     }
 
 }
